9923e499d0bab4c663bee45153683f7622a29422
Muhammad Rizki <118183004+rzki21@users.noreply.github.com>
2025-04-11 20:13:09 +0700

    Update next.config.mjs

--- client-apps/next.config.mjs
+++ client-apps/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    // Inject env variables during build
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_URL: process.env.AUTH_URL, // Hanya untuk server
+  },
+  
+};
 
 export default nextConfig;
